dff79d81329111bcd6e40be29cb635dfd1cd95dc
mattayoungs <mattayoungs@icloud.com>
2020-05-25 12:33:50 -0400

    turned into class comp, added new fav btn

--- src/App.js
+++ src/App.js
@@ -3,41 +3,56 @@ import Dog from "./components/Dog";
 import Header from "./components/Header";
 import "./App.css";
 
-function App() {
-  let dogs = [
-    {
-      name: "Scruffles",
-      type: "Chihuahua",
-    },
-    {
-      name: "Snarf",
-      type: "Border Collie",
-    },
-    {
-      name: "Wookie",
-      type: "English Sheepdog",
-    },
-  ];
-
-  function pickNewFavorite() {
-    let newDogIndex = Math.floor(Math.random() * dogs.length);
-    return dogs[newDogIndex];
+class App extends React.Component {
+  state = {
+    dogs: [
+      {
+        name: "Scruffles",
+        type: "Chihuahua",
+      },
+      {
+        name: "Snarf",
+        type: "Border Collie",
+      },
+      {
+        name: "Wookie",
+        type: "English Sheepdog",
+      },
+    ],
+    favoriteDog: "",
+  };
+  pickNewFavorite() {
+    let newDogIndex = Math.floor(Math.random() * this.state.dogs.length);
+    return this.state.dogs[newDogIndex];
   }
+  // onPickNewFavorite() {
+  //   this.setState({ favoriteDog: this.pickNewFavorite() });
+  // }
+  // componentDidMount() {
+  //   this.setState({ favoriteDog: this.pickNewFavorite() });
+  // }
+  onPickNewFavorite = () => {
+    this.setState((state) => ({
+      favoriteDog: this.pickNewFavorite(),
+    }));
+  };
 
-  let favoriteDog = pickNewFavorite();
-
-  return (
-    <div className="dogs">
-      <Header numDogs={dogs.length} />
-      <h2>My Dogs</h2>
-      <div>
-        {dogs.map((dog) => (
-          <Dog dog={dog} />
-        ))}
+  render() {
+    this.pickNewFavorite();
+    return (
+      <div className="dogs">
+        <Header numDogs={this.state.dogs.length} />
+        <h2>My Dogs</h2>
+        <div>
+          {this.state.dogs.map((dog) => (
+            <Dog dog={dog} />
+          ))}
+        </div>
+        <h2>My Favorite dog is {this.state.favoriteDog.name}</h2>
+        <button onClick={this.onPickNewFavorite}>Pick New Favorite</button>
       </div>
-      <h2>My Favorite dog is {favoriteDog.name}</h2>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
